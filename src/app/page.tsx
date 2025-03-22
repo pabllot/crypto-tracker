073194acfd61a6abfa6359eb4ba6b54fc39c3c6a
Pablo Teixeira <t_pablo.teixeira@hapvida.com.br>
2025-03-22 19:03:51 -0300

    refactor: build changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import ChildComponent from "./components/ChildComponent";
 import CryptoTable from "./components/CryptoTable";
 
+const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` // ✅ Use Vercel URL in production
+  : "http://localhost:3000"; // ✅ Fallback to localhost for local development
+
+
 export default async function Page() {
   const [top10Res, allRes] = await Promise.all([
-    fetch("http://localhost:3000/api/top10", { next: { revalidate: 10 } }),
-    fetch("http://localhost:3000/api/all", { next: { revalidate: 10 } })
+    fetch(`${baseUrl}/api/top10`, { next: { revalidate: 10 } }),
+    fetch(`${baseUrl}//api/all`, { next: { revalidate: 10 } })
   ]);
 
   const [top10Data, allData] = await Promise.all([top10Res.json(), allRes.json()]);
@@ -13,3 +18,5 @@ export default async function Page() {
     <ChildComponent />
   </CryptoTable>;
 }
+
+export const dynamic = "force-dynamic";
