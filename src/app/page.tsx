57b7c6787f14b7e39fd9654b522d19375a6c626d
Pablo Teixeira <t_pablo.teixeira@hapvida.com.br>
2025-03-22 19:09:10 -0300

    fix: typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
 export default async function Page() {
   const [top10Res, allRes] = await Promise.all([
     fetch(`${baseUrl}/api/top10`, { next: { revalidate: 10 } }),
-    fetch(`${baseUrl}//api/all`, { next: { revalidate: 10 } })
+    fetch(`${baseUrl}/api/all`, { next: { revalidate: 10 } })
   ]);
 
   const [top10Data, allData] = await Promise.all([top10Res.json(), allRes.json()]);
